d8cc7daf28bf1240ca4eb811092aae919c41bbfb
Ajeet Jha <kumarjhaajeet653@gmail.com>
2023-08-07 21:21:35 +0530

    react-all-setup-completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Navbar } from 'react-bootstrap'
+function Header() {
+  return <Navbar>
+    <Container>
+      <Navbar.Brand>
+        <a href="/">Shopping Cart</a>
+      </Navbar.Brand>
+    </Container>
+  </Navbar>
+}
+  export default Header;
